6f595a7cec08889f1aa52e9f2494a4a1e5a05714
Felix Becker <git@felixbecker.name>
2012-03-09 14:14:00 +0100

    auf maven umgestellt

--- src/test/java/name/felixbecker/jaxbissuetracker/MarshallingTest.java
+++ src/test/java/name/felixbecker/jaxbissuetracker/MarshallingTest.java
@@ -11,25 +11,32 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
-import name.felixbecker.jaxbissuetracker.xml.ConcreteType;
-import name.felixbecker.jaxbissuetracker.xml.HijackedType;
-import name.felixbecker.jaxbissuetracker.xml.ObjectFactory;
+import name.felixbecker.concrete.ConcreteType;
+import name.felixbecker.concrete.ObjectFactory;
+import name.felixbecker.hijacked.HijackedType;
 
-public class Main {
+import org.junit.Test;
+
+public class MarshallingTest {
 	
-	public static void main(String... args) throws Exception {
-		final Main main = new Main();
-		String xml = main.marshallConcreteType();
+	@Test
+	public void testMarshalling() throws Exception {
+		String xml = marshallConcreteType();
 		System.out.println("XML: " + xml);
-		main.unmarshallConcreteType(xml);
 	}
 	
+	@Test
+	public void testUnmarshalling() throws Exception {
+		unmarshallConcreteType(marshallConcreteType());
+	}
+	
+
 	public ConcreteType unmarshallConcreteType(String xml) throws Exception {
 		JAXBContext context = JAXBContext.newInstance(ConcreteType.class);
 		Unmarshaller unmarshaller = context.createUnmarshaller();
 		StringReader stringReader = new StringReader(xml);
 		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = schemaFactory.newSchema(getClass().getResource("concrete.xsd"));
+		Schema schema = schemaFactory.newSchema(getClass().getResource("/concrete.xsd"));
 		unmarshaller.setSchema(schema);
 		ConcreteType concreteType = (ConcreteType) unmarshaller.unmarshal(stringReader);
 		return concreteType;
@@ -37,32 +44,31 @@ public class Main {
 	}
 	
 	public String marshallConcreteType() throws Exception {
-			
-			StringWriter stringWriter = new StringWriter();
-	
-			// schema for validation
-	
+				
 			JAXBContext context = JAXBContext.newInstance(ConcreteType.class);
 			Marshaller m = context.createMarshaller();
 			
 			// Validation
 			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-			Schema schema = schemaFactory.newSchema(getClass().getResource("concrete.xsd"));
+			Schema schema = schemaFactory.newSchema(getClass().getResource("/concrete.xsd"));
 			m.setSchema(schema);
 	
 			// pretty printing
 			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-			
-			ConcreteType concreteType = new ConcreteType();
-			HijackedType hijackedType = new HijackedType();
-			hijackedType.setValue("foobar");
-			concreteType.setHijacked(hijackedType);
-			
-			JAXBElement<ConcreteType> elem = new ObjectFactory().createConcreteType(concreteType);
-			
-			m.marshal(elem, stringWriter);
-						
+
+			// write marshalled xml to stringwriter
+			StringWriter stringWriter = new StringWriter();
+			m.marshal(getConcreteType(), stringWriter);
 			return stringWriter.toString();
+	}
+	
+	private JAXBElement<ConcreteType> getConcreteType(){
+		ConcreteType concreteType = new ConcreteType();
+		HijackedType hijackedType = new HijackedType();
+		hijackedType.setValue("foobar");
+		concreteType.setHijacked(hijackedType);
+		
+		return new ObjectFactory().createConcreteType(concreteType);
 	}
 
 }
